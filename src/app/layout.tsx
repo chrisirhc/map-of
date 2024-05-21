5e5d93a028fadd77aa6e9c05d4e7a6573bada6e7
Chris Chua <chris.sirhc@gmail.com>
2024-05-21 21:16:53 +0800

    use radix-ui/themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Theme, Text, Link } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,18 +19,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <footer>
-          <div>
-            Data is sourced from{" "}
-            <a href="https://www.singpost.com/locate-us">Singapore Post</a>
-          </div>
-          <div>
-            <a href="https://github.com/chrisirhc/map-of">
-              Source is at Github
-            </a>
-          </div>
-        </footer>
+        <Theme>
+          {children}
+          <footer>
+            <Text>
+              Data is sourced from{" "}
+              <Link href="https://www.singpost.com/locate-us">
+                Singapore Post
+              </Link>
+            </Text>
+            <div>
+              <Link href="https://github.com/chrisirhc/map-of">
+                Source is at Github
+              </Link>
+            </div>
+          </footer>
+        </Theme>
       </body>
     </html>
   );
